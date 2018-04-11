daeb109d4e6d462a8ce4bb5a12ced57c184d0aed
longhowlam <longhowlam@gmail.com>
2018-04-11 13:35:33 +0100

    training

--- Audi_BMW.R
+++ Audi_BMW.R
@@ -3,14 +3,15 @@ library(keras)
 ######## Introduction #########################################################
 
 ## train an AUDI BMW classifier based on images that were downloaded using the 
-## python package google-images-download. Note some images where not usable by keras
+## python package google-images-download. Note: some images that 
+## where downloaded were not usable by keras
 
 ## Do the fast and easy way, create features from a pretrained network
 ## and use thos features in a dense network
 
-######## conv base to create feutures ###############################
+######## conv base to create feutures #########################################
 
-conv_base <- application_vgg16(
+conv_base = application_vgg16(
   weights = "imagenet",
   include_top = FALSE,
   input_shape = c(150, 150, 3)
@@ -19,14 +20,14 @@ conv_base <- application_vgg16(
 
 ######## extract freatures ######################################
 
-train_dir <- file.path("training")
-#validation_dir <- file.path(base_dir, "validation")
-#test_dir <- file.path(base_dir, "test")
+train_dir      = file.path("training")
+validation_dir = file.path("validation")
 
-datagen <- image_data_generator(rescale = 1/255)
-batch_size <- 20
 
-extract_features <- function(directory, sample_count) {
+datagen = image_data_generator(rescale = 1/255)
+batch_size = 20
+
+extract_features = function(directory, sample_count) {
   
   features <- array(0, dim = c(sample_count, 4, 4, 512))  
   labels <- array(0, dim = c(sample_count))
@@ -63,12 +64,19 @@ extract_features <- function(directory, sample_count) {
   )
 }
 
-train <- extract_features(
+train = extract_features(
   directory = train_dir, 
-  sample_count = 120
+  sample_count = 200
 )
 
 table(train$labels)
+
+validation = extract_features(
+  directory = validation_dir, 
+  sample_count = 80
+)
+
+table(validation$labels)
 
 ############ reshape feautures ######################################
 
@@ -77,6 +85,7 @@ reshape_features <- function(features) {
 }
 
 train$features <- reshape_features(train$features)
+validation$features <- reshape_features(validation$features)
 
 
 ########## train the model #############################################
@@ -105,17 +114,81 @@ history <- model %>%
     train$features,
     train$labels,
     epochs = 30,
-    batch_size = 20
-    #,    validation_data = list(validation$features, validation$labels)
+    batch_size = 20,
+    validation_data = list(validation$features, validation$labels)
 )
 
 plot(history)
 
+### not a high accuracy
 
 
 
+######  extended model ########################################################
+
+model <- keras_model_sequential() %>% 
+  conv_base %>% 
+  layer_flatten() %>% 
+  layer_dense(
+    units = 256, activation = "relu"
+  ) %>% 
+  layer_dense(
+    units = 1, activation = "sigmoid"
+  )
+
+summary(model)
 
 
+#### freezing weights ########################################################
+cat("This is the number of trainable weights before freezing",
+    "the conv base:", length(model$trainable_weights), "\n")
+freeze_weights(conv_base)
+cat("This is the number of trainable weights after freezing",
+    "the conv base:", length(model$trainable_weights), "\n")
 
-validation <- extract_features(validation_dir, 1000)
-test <- extract_features(test_dir, 1000)
+
+############  data augmentation and model training ############################
+train_datagen = image_data_generator(
+  rescale = 1/255,
+  rotation_range = 40,
+  width_shift_range = 0.2,
+  height_shift_range = 0.2,
+  shear_range = 0.2,
+  zoom_range = 0.2,
+  horizontal_flip = TRUE,
+  fill_mode = "nearest"
+)
+
+test_datagen <- image_data_generator(rescale = 1/255)
+
+train_generator <- flow_images_from_directory(
+  train_dir,
+  train_datagen,
+  target_size = c(150, 150),
+  batch_size = 20,
+  class_mode = "binary"
+)
+
+validation_generator <- flow_images_from_directory(
+  validation_dir,
+  test_datagen,
+  target_size = c(150, 150),
+  batch_size = 20,
+  class_mode = "binary"
+)
+
+model %>% compile(
+  loss = "binary_crossentropy",
+  optimizer = optimizer_rmsprop(lr = 2e-5),
+  metrics = c("accuracy")
+)
+
+history <- model %>% fit_generator(
+  train_generator,
+  steps_per_epoch = 100,
+  epochs = 30,
+  validation_data = validation_generator,
+  validation_steps = 50
+)
+
+
